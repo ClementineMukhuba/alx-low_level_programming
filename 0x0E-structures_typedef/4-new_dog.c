8cec36eee9eee5767fb9c043a1563b376857b6ef
ClementineMukhuba <135021854+ClementineMukhuba@users.noreply.github.com>
2023-07-30 13:56:46 -0700

    4c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,15 +9,15 @@
  */
 
 int _strlen(char *s)
-{       
-        int c;
-        c = 0;
-        while (s[c] != '\0')
-        {       
-                c++;
-        }       
-        return (c);
-}       
+{
+	int c;
+	c = 0;
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	return (c);
+}
 
 /**
  * _strcpy - Copies the string
