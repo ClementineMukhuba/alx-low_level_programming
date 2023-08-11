f62e3f50acce1e9546cafc87fd7eab194708ce92
ClementineMukhuba <135021854+ClementineMukhuba@users.noreply.github.com>
2023-08-11 12:39:05 -0700

    Update1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
-/*
- * create_file - A function that creates a file
+/**
+ * create_file - creates a file.
  * @filename: Name of the file to create
  * @text_content: A NULL terminated string to write to the file.
  *
@@ -9,12 +9,12 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fde, res =1, countn = 0;
+	int fde, res = 1, countn = 0;
 
 	if (!filename)
 		return (-1);
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (file == -1)
+	fde = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fde == -1)
 		return (-1);
 
 	if (text_content)
