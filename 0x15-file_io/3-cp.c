a2d761184007fe2855711721727888e731bfce7d
ClementineMukhuba <135021854+ClementineMukhuba@users.noreply.github.com>
2023-08-11 15:20:52 -0700

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,28 @@
 #include "main.h"
-#define BUFF_SIZE 1024
+#include <stdio.h>
+
+/**
+ * error_file - checks errors
+ * @from: file from 
+ * @to: file to
+ * @argv: Arguments path
+ *
+ */
+void error_file(int from, int to, char *argv[])
+{
+	if(from == -1)
+	{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	if(to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
 
 /**
  * main - copies the content of a file to another file.
@@ -8,95 +31,50 @@
  *
  * Return: Always 0
  */
-
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int from, to, result0, result1;
-	char *buffer;
+	int from, to, error_close;
+	ssize_t reschar, reswr;
+	char buff[1024];
 
-	if (argc != 3)
+	if(argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
 
-	buffer = malloc(sizeof(char) *BUFF_SIZE);
-	if (!buffer)
-		return (0);
-
 	from = open(argv[1], O_RDONLY);
-	error_98(from, buffer, argv[1]);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	error_99(to, buffer, argv[2]);
 
-	do {
-		result0 = read(from, buffer, BUFF_SIZE);
-		if (result0 == 0)
-			break;
-		error_98(from, buffer, argv[1])
-		result1 = write(to, buffer, result0);
-		error_99(to, buffer, argv[2]);
-	} while (result 1 >= BUFF_SIZE);
-	
-	result0 = close(from);
-	error_100(result0, buffer);
-	result0 = close(to);
-	error_100(result0, buffer);
+	to = open(argv[2], O-CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(from, to, argv);
 
-	free(buffer);
-	return (0);
-}
+	reschar = 1024;
 
-
-/**
- * error_98 - file_from error if doesnt exist or cant be read
- * @from: Value to be checked.
- * @buffer: The Buffer.
- * @argv: An argument
- *
- */
-void error_98(int from, char *buffer, char *argv)
-{
-	if (from < 0)
+	while(reschar == 1024)
 	{
-		dprintf(STDERR_FILENO,"Error: Can't read from file %s\n", argv[1]);
-			free(buffer);
-			exit(98);
-
+		reschar = read(from, buff, 1024);
+		if(reschar == -1)
+			error_file(-1, 0, argv);
+		reswr = write(to, buff, reschar);
+		if(reswr == -1)
+			error_file(0, -1, argv);
 	}
-}
+	error_close = close(from);
 
-
-/**
- * error_99 - file_to error if cant create or write
- * @from: Value to be checked.
- * @buffer: The Buffer.
- * @argv: An argument
- *
- */
-void error_99(int from, char *buffer, char *argv)
-{
-	if (from < 0)
+	if(error_close == -1)
 	{
-		dprintf(STDERR_FILENO,"Error: Can't write to %s\n", argv[2]);
-                        free(buffer);
-                        exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
+		exit(100);
 	}
-}
 
+	error_close = close(to);
 
-/**
- * error_100 - can't close file descriptor error
- * @from: Value to be checked.
- * @buffer: The Buffer.
- *
- */
-void error_100(int from, char *buffer)
-{
-	if (from < 0)
+	if(error_close == -1)
 	{
-		dprintf(STDERR_FILENO,"Error: Can't close fde %i\n", from);
-                        free(buffer);
-                        exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to);
+		exit(100);
 	}
+	return(0);
 }
+
+
