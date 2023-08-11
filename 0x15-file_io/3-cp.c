cca42f33816191b5b53e4823fef5ca4054e17fc0
ClementineMukhuba <135021854+ClementineMukhuba@users.noreply.github.com>
2023-08-11 15:28:55 -0700

    another update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,13 +10,13 @@
  */
 void error_file(int from, int to, char *argv[])
 {
-	if(from == -1)
+	if (from == -1)
 	{
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	if(to == -1)
+	if (to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
@@ -37,43 +37,38 @@ int main(int argc, char *argv[])
 	ssize_t reschar, reswr;
 	char buff[1024];
 
-	if(argc != 3)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
 
 	from = open(argv[1], O_RDONLY);
-
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(from, to, argv);
-
 	reschar = 1024;
 
-	while(reschar == 1024)
+	while (reschar == 1024)
 	{
 		reschar = read(from, buff, 1024);
-		if(reschar == -1)
+		if (reschar == -1)
 			error_file(-1, 0, argv);
 		reswr = write(to, buff, reschar);
-		if(reswr == -1)
+		if (reswr == -1)
 			error_file(0, -1, argv);
 	}
 	error_close = close(from);
-
-	if(error_close == -1)
+	if (error_close == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
 		exit(100);
 	}
-
 	error_close = close(to);
-
-	if(error_close == -1)
+	if (error_close == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to);
 		exit(100);
 	}
-	return(0);
+	return (0);
 }
 
